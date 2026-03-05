1843d32629c0a6e938d95975e0890e91c9ccab59
Alexey Gladkov <gladkov.alexey@gmail.com>
2026-03-05 10:19:10 +0100

    Allow to use some targets without git repository
    Some targets do not require the configuration stored in git-config. If
    variable definitions are moved, unnecessary utility execution and
    unnecessary checks can be avoided.
    
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- devkit.mk
+++ devkit.mk
@@ -9,6 +9,9 @@ VERSION = 1
 V = $(VERBOSE)
 Q = $(if $(V),,@)
 
+SIMPLE_GOALS = clean clean-all list help version
+PUBLIC_GOALS = $(SIMPLE_GOALS) init check upgrade shell run
+
 define require-utility
 $(eval $(1) := $(shell command -v $(2) 2>/dev/null))
 $(if $($(1)),,$(error Required utility '$(2)' not found))
@@ -18,38 +21,6 @@ $(call require-utility,GIT,git)
 $(call require-utility,PODMAN,podman)
 $(call require-utility,CURL,curl)
 
-GITPROJDIR = $(shell $(GIT) rev-parse --show-toplevel 2>/dev/null)
-PROJNAME   = $(notdir $(GITPROJDIR))
-
-ifeq ($(filter help clean clean-all list,$(MAKECMDGOALS)),)
-$(if $(PROJNAME),,$(error Unable to locate the git repository))
-endif
-
-UID := $(shell id -u)
-GID := $(shell id -g)
-
-DEF_AGENT = copilot
-DEF_DEVNAME = $(PROJNAME)
-DEF_DEVSHELL = /bin/bash
-DEF_EDITOR = /usr/bin/editor
-
-ifneq ($(GITPROJDIR),)
-VENDOR  = ubuntu
-AGENT   = $(shell $(GIT) config get       devkit.agent    || echo $(DEF_AGENT))
-DEVNAME = $(shell $(GIT) config get       devkit.name     || echo $(DEF_DEVNAME))
-DEVSHELL= $(shell $(GIT) config get       devkit.shell    || echo $(DEF_DEVSHELL))
-EDITOR  = $(shell $(GIT) config get       devkit.editor   || echo $(DEF_EDITOR))
-DEVPKGS = $(shell $(GIT) config get --all devkit.packages)
-VOLUMES = $(shell $(GIT) config get --all devkit.volumes)
-SHAHASH = $(shell echo $(UID):$(GID) $(AGENT) $(VENDOR) $(sort $(DEVPKGS)) | sha256sum | cut -f1 -d\ )
-endif
-
-get-image-id       = $(shell $(PODMAN) image list --filter label=local.devkit.hash=$(SHAHASH) --format '{{.Id}}')
-get-github-release = $(shell $(CURL) -fsSL -o /dev/null -w '%{url_effective}' '$(HOMEURL)' | sed -n 's,.*/tag/v\?,,p')
-
-ubuntu.packages.npm = npm
-ubuntu.packages.scr = bash curl
-
 AGENT.opencode = HOMEURL=https://github.com/anomalyco/opencode/releases/latest       INST=scr LINK=https://opencode.ai/install   BIN=opencode CONFDIR=.config/opencode
 AGENT.copilot  = HOMEURL=https://github.com/github/copilot-cli/releases/latest       INST=scr LINK=https://gh.io/copilot-install BIN=copilot  CONFDIR=.copilot
 AGENT.claude   = HOMEURL=https://github.com/anthropics/claude-code/releases/latest   INST=scr LINK=https://claude.ai/install.sh  BIN=claude   CONFDIR=.claude
@@ -58,18 +29,45 @@ AGENT.gemini   = HOMEURL=https://github.com/google-gemini/gemini-cli/releases/la
 AGENT.codex    = HOMEURL=https://github.com/openai/codex/releases/latest             INST=npm LINK=@openai/codex                 BIN=codex    CONFDIR=.codex
 AGENT.grok     = HOMEURL=https://github.com/superagent-ai/grok-cli/releases/latest   INST=npm LINK=@vibe-kit/grok-cli            BIN=grok     CONFDIR=.grok
 
+ifeq ($(filter $(SIMPLE_GOALS),$(MAKECMDGOALS)),) # not SIMPLE_GOALS
+GITPROJDIR = $(shell $(GIT) rev-parse --show-toplevel 2>/dev/null)
+PROJNAME   = $(notdir $(GITPROJDIR))
+
+$(if $(PROJNAME),,$(error Unable to locate the git repository))
+
+DEF_AGENT = copilot
+DEF_DEVNAME = $(PROJNAME)
+DEF_DEVSHELL = /bin/bash
+DEF_EDITOR = /usr/bin/editor
+
+VENDOR  = ubuntu
+AGENT   = $(shell $(GIT) config get       devkit.agent    || echo $(DEF_AGENT))
+DEVNAME = $(shell $(GIT) config get       devkit.name     || echo $(DEF_DEVNAME))
+DEVSHELL= $(shell $(GIT) config get       devkit.shell    || echo $(DEF_DEVSHELL))
+EDITOR  = $(shell $(GIT) config get       devkit.editor   || echo $(DEF_EDITOR))
+DEVPKGS = $(shell $(GIT) config get --all devkit.packages)
+VOLUMES = $(shell $(GIT) config get --all devkit.volumes)
+SHAHASH = $(shell echo $(UID):$(GID) $(AGENT) $(VENDOR) $(sort $(DEVPKGS)) | sha256sum | cut -f1 -d\ )
+
 ifeq ($(strip $(AGENT.$(AGENT))),)
 $(error Unknown devkit.agent '$(AGENT)'. Supported: aider, claude, codex, copilot, gemini, opencode, grok)
 endif
 
 $(foreach f,HOMEURL INST LINK BIN CONFDIR,$(eval $(f)=$(patsubst $(f)=%,%,$(filter $(f)=%,$(AGENT.$(AGENT))))))
 
-.PHONY: _create-image-$(VENDOR) help version init clean clean-all check upgrade list shell run
+get-image-id       = $(shell $(PODMAN) image list --filter label=local.devkit.hash=$(SHAHASH) --format '{{.Id}}')
+get-github-release = $(shell $(CURL) -fsSL -o /dev/null -w '%{url_effective}' '$(HOMEURL)' | sed -n 's,.*/tag/v\?,,p')
+
+UID := $(shell id -u)
+GID := $(shell id -g)
+endif # not SIMPLE_GOALS
+
+.PHONY: _create-image-ubuntu $(PUBLIC_GOALS)
 .ONESHELL:
 
 help:
 	@echo ""
-	echo "Usage: $(PROG) [ help$(foreach x,version init clean check upgrade list shell run, | $(x)) ]"
+	echo "Usage: $(PROG) [ $(strip $(subst ||,,|$(addprefix | ,$(PUBLIC_GOALS)))) ]"
 	echo ""
 	echo "The project allows you to manage isolated containers with AI agents."
 	echo ""
@@ -104,6 +102,9 @@ init:
 	else
 	  echo "Discovered the existing configuration and cowardly refuse to break it." >&2;
 	fi
+
+ubuntu.packages.npm = npm
+ubuntu.packages.scr = bash curl
 
 _create-image-ubuntu: $(if $(filter upgrade,$(MAKECMDGOALS)),clean)
 	$(Q)[ -n "$(get-image-id)" ] || printf '%s\n' \
